65eacf09ce336b4c394083f9726d2c9de3392e3e
AdilMir1433 <adil.mir@devsinc.com>
2023-08-02 21:37:26 +0500

    user service front end fixed

--- src/main/java/com/example/examservice/entities/Exam.java
+++ src/main/java/com/example/examservice/entities/Exam.java
@@ -4,26 +4,27 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
-import lombok.RequiredArgsConstructor;
+import lombok.*;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.sql.Time;
 import java.util.Date;
 
 @Entity
 @AllArgsConstructor
 @NoArgsConstructor
 @Transactional
+@Getter
+@Setter
 
 public class Exam {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     String examTitle;
+    Time startTime;
+    Time endTime;
     Date startDate;
-    Date endDate;
-    String examDuration;
     Long subjectID;
     Long teacherID;
     boolean isActivated;
--- src/main/java/com/example/examservice/entities/Subject.java
+++ src/main/java/com/example/examservice/entities/Subject.java
@@ -2,6 +2,8 @@ package com.example.examservice.entities;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -17,6 +19,7 @@ import org.springframework.transaction.annotation.Transactional;
 @Setter
 public class Subject {
     @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     @Column(unique = true)
     String subjectName;
